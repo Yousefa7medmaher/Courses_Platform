36fa1445418f035104a303636d73bb359e7b3a00
Yousefa7medmaher <ya1770620@gmail.com>
2025-07-29 08:08:04 +0300

    feat: update user routes to include change password endpoint

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { adduser  , showAllUsers ,updateUser } from './controllers/userApi.js';
+import { adduser  , showAllUsers ,updateUser , changePassword } from './controllers/userApi.js';
 
 router.post('/api/addUser',  adduser); 
 router.get('/api/users',  showAllUsers); 
 router.put('/api/updateUser/:id',  updateUser); 
-router.patch('/api/changePassword/:id',  updateUser); 
+router.patch('/api/changePassword/:id',  changePassword); 
 
 export default router;
