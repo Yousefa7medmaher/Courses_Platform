a5b6d69a3b6e75da357bdb6289736af92124616a
Yousefa7medmaher <ya1770620@gmail.com>
2025-08-02 07:56:09 +0300

    chore(https): start configuring HTTPS support for secure connections

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 import connectDB from './db.js';
 import router from './router.js';
 import cookieParser from 'cookie-parser';
-
 
 dotenv.config();
 
@@ -12,18 +13,16 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-// Enable CORS for all routes and allow requests from the frontend and admin panel
+// Enable CORS
 const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
-  'http://localhost:3000',      // frontend React مثلاً
+  'http://localhost:3000',
   'http://127.0.0.1:3000',
 ];
 
-
 app.use(cors({
   origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
@@ -34,18 +33,22 @@ app.use(cors({
   credentials: true
 }));
 
-// Connect routing
+// Routes
 app.use(router);
 
-/**
- * Start Server
- */
 const PORT = process.env.PORT || 5000;
+
+// Load HTTPS certificates
+const httpsOptions = {
+  key: fs.readFileSync('./certs/localhost-key.pem'),
+  cert: fs.readFileSync('./certs/localhost.pem'),
+};
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+
+  https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`🔐 HTTPS Server running at https://localhost:${PORT}`);
     console.log(`🌐 CORS enabled for: ${allowedOrigins.join(', ')}`);
   });
 };
